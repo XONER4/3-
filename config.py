d13c12658bd110a3a77b8afcc5608a040736661a
XONER4 <vladislavponomarev16@icloud.com>
2026-04-12 21:29:09 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+# Токен твоего бота (вставь сюда)
+BOT_TOKEN = "6080982996:AAFh_k0uNnYjGlIS8lnrAOt_sKxsLIa3v38"
+
+# ID твоего аккаунта в Telegram (администратора)
+ADMIN_ID = 687370909
+
+# Пароль для входа в бота
+BOT_PASSWORD = "274452"
+
+# Начальный баланс для новых пользователей
+START_BALANCE = 15000
+
+# Настройки для ежедневного бонуса
+DAILY_BONUS = 3000
+
+# Настройки для кредита
+CREDIT_PERCENT = 10  # 10%
+CREDIT_MAX_DAYS = 3
