cdae2a229a6bd10d4462164028c64903d61ea147
XONER4 <vladislavponomarev16@icloud.com>
2026-04-13 14:48:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,10 @@
 import os
 
-# Токен твоего бота
-BOT_TOKEN = "6080982996:AAFtSreoS1th7l4iFvKnqqelnL5FWz5Vo7A"
-
-# ID твоего аккаунта в Telegram (администратора)
+BOT_TOKEN = "ВАШ_ТОКЕН_ЗДЕСЬ"  # замените на актуальный
 ADMIN_ID = 687370909
-
-# Пароль для входа в бота
 BOT_PASSWORD = "274452"
-
-# Начальный баланс для новых пользователей
 START_BALANCE = 15000
-
-# Настройки для ежедневного бонуса
-DAILY_BONUS = 3000
-
-# Настройки для кредита
-CREDIT_PERCENT = 10  # 10%
+DAILY_BONUS_BASE = 3000
+CREDIT_PERCENT = 10
 CREDIT_MAX_DAYS = 3
+TIMEZONE_OFFSET = 3  # МСК
