aae59fe1fb5fd25ebd71a91bd8eba0c97a80e731
XONER4 <vladislavponomarev16@icloud.com>
2026-04-17 16:28:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-BOT_TOKEN = "6080982996:AAFtSreoS1th7l4iFvKnqqelnL5FWz5Vo7A"  # замените на актуальный
-BOT_USERNAME = "Ruferutyretywbot"  # юзернейм бота без @
+BOT_TOKEN = "6080982996:AAFtSreoS1th7l4iFvKnqqelnL5FWz5Vo7A"
+BOT_USERNAME = "Xoner4Bot"
 ADMIN_ID = 687370909
-NEWS_CHANNEL_ID = -1002391156112  # числовой ID канала (узнать через @getmyid_bot)
-NEWS_CHANNEL_USERNAME = "novostibots13"  # юзернейм канала без @
+NEWS_CHANNEL_ID = -1003911156112      # ← правильный ID
+NEWS_CHANNEL_USERNAME = "novostibots13"
 BOT_PASSWORD = "274452"
 START_BALANCE = 15000
 DAILY_BONUS_BASE = 3000
 CREDIT_PERCENT = 10
 CREDIT_MAX_DAYS = 3
-TIMEZONE_OFFSET = 3  # МСК
+TIMEZONE_OFFSET = 3
 MAIN_MENU_TEXT = "👋 Привет, {name}! Сегодня {date}.\nВыберите действие:"
