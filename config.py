2168b846bf508b122b27d0fccf11c226a62f0db0
XONER4 <vladislavponomarev16@icloud.com>
2026-04-17 08:58:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 BOT_TOKEN = "6080982996:AAFtSreoS1th7l4iFvKnqqelnL5FWz5Vo7A"  # замените на актуальный
-BOT_USERNAME = "Ruferutyretywbot"  # укажи юзернейм бота без @, например Xoner4Bot
+BOT_USERNAME = "Ruferutyretywbot"  # юзернейм бота без @
 ADMIN_ID = 687370909
-NEWS_CHANNEL_ID = -3911156112  # ID канала новостей (числовой, можно получить через @getidsbot)
+NEWS_CHANNEL_ID = -1002391156112  # числовой ID канала (узнать через @getmyid_bot)
+NEWS_CHANNEL_USERNAME = "novostibots13"  # юзернейм канала без @
 BOT_PASSWORD = "274452"
 START_BALANCE = 15000
 DAILY_BONUS_BASE = 3000
